70878287b5ad23137ed67fd5d90407741b112d8c
egyardias <egyardiassolin@students.polmed.ac.id>
2025-01-16 16:45:10 +0700

    app,home.module.css,index,footer,navbar,about

---
+++ frontend/component/footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#eee' }}>
+        <p>&copy; 2025 My App</p>
+      </footer>
+    );
+  }
+  
