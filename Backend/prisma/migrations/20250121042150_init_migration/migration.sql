7486b7451db0a1da1df617127bbe6e42cf1871b4
pahutar <sipahutar0104@gmai.com>
2025-01-21 11:26:30 +0700

    penggantian mysql ke postgreSQL

---
+++ Backend/prisma/migrations/20250121042150_init_migration/migration.sql
@@ -0,0 +1,129 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "number" TEXT NOT NULL,
+    "contractNumber" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "requestBy" TEXT NOT NULL,
+    "isConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "isReceived" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderDetails" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "materialId" INTEGER NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "mention" TEXT NOT NULL,
+
+    CONSTRAINT "OrderDetails_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Materials" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "image" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "vendorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Materials_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vendors" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT,
+    "city" TEXT,
+    "phone" TEXT,
+
+    CONSTRAINT "Vendors_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Purchases" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "number" TEXT NOT NULL,
+    "refrence" TEXT NOT NULL,
+    "subtotal" INTEGER NOT NULL,
+    "tax" INTEGER NOT NULL,
+    "totalPayment" INTEGER NOT NULL,
+    "payment" TEXT NOT NULL,
+    "createBy" TEXT NOT NULL,
+    "checkByFinance" BOOLEAN NOT NULL DEFAULT false,
+    "isAccepted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Purchases_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DetailPurchases" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "purchaseId" INTEGER NOT NULL,
+    "materialId" INTEGER NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "codeBudget" TEXT NOT NULL,
+    "mention" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    "totalPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "DetailPurchases_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Order_number_key" ON "Order"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Categories_name_key" ON "Categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Purchases_number_key" ON "Purchases"("number");
+
+-- AddForeignKey
+ALTER TABLE "OrderDetails" ADD CONSTRAINT "OrderDetails_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderDetails" ADD CONSTRAINT "OrderDetails_materialId_fkey" FOREIGN KEY ("materialId") REFERENCES "Materials"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materials" ADD CONSTRAINT "Materials_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materials" ADD CONSTRAINT "Materials_vendorId_fkey" FOREIGN KEY ("vendorId") REFERENCES "Vendors"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DetailPurchases" ADD CONSTRAINT "DetailPurchases_purchaseId_fkey" FOREIGN KEY ("purchaseId") REFERENCES "Purchases"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DetailPurchases" ADD CONSTRAINT "DetailPurchases_materialId_fkey" FOREIGN KEY ("materialId") REFERENCES "Materials"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DetailPurchases" ADD CONSTRAINT "DetailPurchases_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
