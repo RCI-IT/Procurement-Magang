31639fd24356103420322170a7ad46d305d449d0
egyardias <egyardiassolin@students.polmed.ac.id>
2025-02-27 14:10:20 +0700

    tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./app//*.{js,ts,jsx,tsx,mdx}",
-    "./component//*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./component/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
+        primary: "#1E40AF",  // Warna biru 
+        secondary: "#E11D48", // Warna merah 
         "gray-800": "#2d2d2d",
         "gray-900": "#1f1f1f",
-        "blue-500": "#1d4ed8",
         "red-600": "#dc2626",
         "red-700": "#b91c1c",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Menambahkan font Poppins
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
