bc578f5780dc74cd0a47445972197c22e8ca0b10
egyardias <egyardiassolin@students.polmed.ac.id>
2025-02-27 11:16:12 +0700

    update vendor

--- frontend/app/material/[id]/page.js
+++ frontend/app/material/[id]/page.js
@@ -90,7 +90,7 @@ export default function MaterialPage() {
           onClick={() => router.push("/material")}
           className="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-gray-600 mb-4"
         >
-          ← Kembali ke Material
+           Kembali ke Material
         </button>
 
         {/* Vendor Info */}
