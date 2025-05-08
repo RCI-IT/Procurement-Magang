9ac15077ddeb9100ee1a74a923ad3350358f8a63
pahutar <sipahutar0104@gmai.com>
2025-05-08 10:02:27 +0700

    update

--- frontend/component/LoadingIcon.tsx
+++ frontend/component/LoadingIcon.tsx
@@ -25,10 +25,6 @@ const LoadingIcon = () => {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-<<<<<<< HEAD
-=======
-
->>>>>>> c90ce9c2e07274ee99bb09cc8b9af9b2b5beb806
             <animate
               attributeName="stroke-dashoffset"
               from="100"
