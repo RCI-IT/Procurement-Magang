c01b99f76e7e2aa208951a7e50876ae2d3332eab
RCI-IT <rcifigma@gmail.com>
2025-05-16 17:07:34 +0700

    auth

--- frontend/services/authServices.js
+++ frontend/services/authServices.js
@@ -1,9 +1,13 @@
 // services/authService.js
+'use client'
+
+let accessToken = null;
 
 export const login = async (username, password) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
+      credentials: "include", // penting agar cookie (refreshToken) dikirim
       body: JSON.stringify({ username, password }),
     });
   
